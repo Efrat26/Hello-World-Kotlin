0860fada418a87db0ce09aab67600bd2187cb571
efiso <efisofer26@gmail.com>
2019-12-04 11:43:37 +0200

    first commit

---
+++ app/src/main/java/com/example/helloworldkotlin/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.helloworldkotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val textView : TextView = findViewById(R.id.hello)
+        textView.setText("hello world!")
+
+    }
+}
+
